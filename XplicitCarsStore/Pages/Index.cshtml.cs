5997bc8d68fedf7db287c81a63ed0f5a93a78f92
johnny.kikambi <J.Kikambi@p-c-m.de>
2019-02-04 12:51:01 +0100

    Add project files.

---
+++ XplicitCarsStore/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace XplicitCarsStore.Pages
+{
+    using System.Net.Http;
+
+    using Newtonsoft.Json;
+
+    public class IndexModel : PageModel
+    {
+        private readonly HttpClient client;
+
+       public string[] Values { get; set; }
+
+        public IndexModel(IHttpClientFactory clientFactory)
+        {
+            this.client = clientFactory.CreateClient("sales");
+        }
+
+        public async Task OnGet()
+        {
+            var response = await this.client.GetAsync("/api/values");
+            var content = await response.Content.ReadAsStringAsync();
+            this.Values = JsonConvert.DeserializeObject<string[]>(content);
+        }
+    }
+}
