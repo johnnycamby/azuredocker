2b46db32032b4e83970f3edb857a20dbda6b1d89
johnny.kikambi <J.Kikambi@p-c-m.de>
2019-02-04 16:23:42 +0100

    azure ad & b2c

---
+++ XplicitCarAzureAD/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace XplicitCarAzureAD.Pages
+{
+    using System.IO.Pipes;
+
+    using XplicitCarAzureAD.Services;
+
+    public class IndexModel : PageModel
+    {
+        private XplicitApiClient client;
+
+        public string[] Values { get; set; }
+
+        public IndexModel(XplicitApiClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task OnGet()
+        {
+            Values = await this.client.GetValues();
+        }
+    }
+}
